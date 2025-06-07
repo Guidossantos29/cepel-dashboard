3a479a4e6b60e9f75bd2bce639ba73de30e55972
Luiz Guilherme <lguidossantos29@gmail.com>
2025-06-07 14:56:22 -0300

    chore: entered application data

---
+++ cepel-dashboard/src/types/dashboard.ts
@@ -0,0 +1,30 @@
+export interface Atendente {
+  "grupo/atendente": string;
+  pendenteIncidente: number;
+  pendenteRequisicao: number;
+  atendimentoIncidente: number;
+  atendimentoRequisicao: number;
+  vencidoIncidente: number;
+  vencidoRequisicao: number;
+  demanda: number;
+}
+
+export interface Grupo {
+  "grupo/atendente": string;
+  pendenteIncidente: number;
+  pendenteRequisicao: number;
+  atendimentoIncidente: number;
+  atendimentoRequisicao: number;
+  vencidoIncidente: number;
+  vencidoRequisicao: number;
+  demanda: number;
+  atendentes: Atendente[];
+}
+
+export interface DadosGerais {
+  novoTotal: number;
+  semAtribuicaoTotal: number;
+  vencidoTotal: number;
+  pendenteTotal: number;
+  sla_geral: string;
+}
