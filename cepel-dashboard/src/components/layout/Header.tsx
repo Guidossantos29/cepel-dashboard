252dd54ebbd1b00ffd1dc141d62c5342303a6f53
Luiz Guilherme <lguidossantos29@gmail.com>
2025-06-08 23:17:13 -0300

    refactor: improving data organization and consuming directly from json

--- cepel-dashboard/src/components/layout/Header.tsx
+++ cepel-dashboard/src/components/layout/Header.tsx
@@ -1,41 +1,67 @@
 import Logo from "../../assets/logo-cepel.png"
 import Resume from "./Resume"
-import { Calendar,MailPlus,UserRoundX,Pause,CircleAlert } from "lucide-react"
-
+import { Calendar, MailPlus, UserRoundX, Pause, CircleAlert } from "lucide-react"
+import dadosGerais from "../../data/dadosGeral.json"
 
 function Header() {
-    return (
-        <>
-            <div className="ml-2 mr-2 flex justify-end ">
-                <span className="text-gray-500">6/5/2025,7:01:49</span>
-            </div>
-            <header >
-                <div className="flex flex-col items-center ml-2 mr-2 md:justify-normal md:flex-row ">
-                    <div className="flex border-b border-b-gray-200 pb-2 mb-3 m-6 justify-center w-[125px] md:border-none ">
-                        <img src={Logo} alt="logo-cepel " />
-                    </div>
-                    <div className="w-full flex flex-col gap-2 mr-3 ml-3 md:flex-row  ">
-                        <Resume label={"1"} value={"Novos"} separator={true} bgColor="bg-resume-green" bgColorIcon="bg-resume-green-dark" icon={<MailPlus/>} />
-                        <Resume label={"4"} value={"Sem atribuições"} separator={true} bgColor="bg-resume-orange" bgColorIcon="bg-resume-orange-dark" icon={<UserRoundX/>} />
-                        <Resume label={"1"} value={"Atrasados"} separator={true} bgColor="bg-resume-red" bgColorIcon="bg-resume-red-dark" icon={<Pause/>} />
-                        <Resume label={"27"} value={"Pendentes"} separator={true} bgColor="bg-resume-yellow" bgColorIcon="bg-resume-yellow-dark" icon={<CircleAlert/>}/>
-                        <Resume label={"100%"} value={"SLA geral(Mês)"} separator={false}  bgColor="bg-resume-blue" bgColorIcon="bg-Resume-blue-dark" icon={<Calendar/>} />
-                    </div>
+  return (
+    <>
+      <div className="ml-2 mr-2 flex justify-end">
+        <span className="text-gray-500">6/5/2025, 7:01:49</span>
+      </div>
 
-                </div>
+      <header>
+        <div className="flex flex-col items-center ml-2 mr-2 md:justify-normal md:flex-row">
+          <div className="flex border-b border-b-gray-200 pb-2 mb-3 m-6 justify-center w-[125px] md:border-none">
+            <img src={Logo} alt="logo-cepel" />
+          </div>
 
-
-
-
-            </header>
-
-
-        </>
-    )
+          <div className="w-full flex flex-col gap-2 mr-3 ml-3 md:flex-row">
+            <Resume
+              label="Novos"
+              value={dadosGerais.novoTotal}
+              separator
+              bgColor="bg-resume-green"
+              bgColorIcon="bg-resume-green-dark"
+              icon={<MailPlus />}
+            />
+            <Resume
+              label="Sem atribuições"
+              value={dadosGerais.semAtribuicaoTotal}
+              separator
+              bgColor="bg-resume-orange"
+              bgColorIcon="bg-resume-orange-dark"
+              icon={<UserRoundX />}
+            />
+            <Resume
+              label="Atrasados"
+              value={dadosGerais.vencidoTotal}
+              separator
+              bgColor="bg-resume-red"
+              bgColorIcon="bg-resume-red-dark"
+              icon={<Pause />}
+            />
+            <Resume
+              label="Pendentes"
+              value={dadosGerais.pendenteTotal}
+              separator={true}
+              bgColor="bg-resume-yellow"
+              bgColorIcon="bg-resume-yellow-dark"
+              icon={<CircleAlert />}
+            />
+            <Resume
+              label="SLA Geral (Mês)"
+              value={`${dadosGerais.sla_geral}%`}
+              separator={false}
+              bgColor="bg-resume-blue"
+              bgColorIcon="bg-resume-blue-dark"
+              icon={<Calendar />}
+            />
+          </div>
+        </div>
+      </header>
+    </>
+  )
 }
 
 export default Header
-
-
-
-
--- cepel-dashboard/src/components/layout/Resume.tsx
+++ cepel-dashboard/src/components/layout/Resume.tsx
@@ -9,32 +9,33 @@ interface ResumeProps {
     separator: boolean;
 }
 
-function Resume({ label, value, icon = "?", bgColor = "bg-gray-200", bgColorIcon = "bg-gray-200", separator }: ResumeProps) {
+function Resume({
+    label,
+    value,
+    icon = "?",
+    bgColor = "bg-gray-200",
+    bgColorIcon = "bg-gray-200",
+    separator,
+}: ResumeProps) {
     return (
         <>
-            <Card className={`${bgColor} w-full h-[80px] md:h-[70px] border-none shadow-sm flex justify-center `}>
-                <CardContent className="flex justify-between items-center">
-                    <div className="flex items-center">
+            <Card className={`${bgColor} flex-1 h-[80px] md:h-[70px] border-none shadow-sm flex justify-center`}>
+                <CardContent className="flex gap-2 justify-between items-center w-full px-4">
+                    <div>
                         <span className={`${bgColorIcon} rounded-full w-10 h-10 flex items-center justify-center font-bold`}>
                             {icon}
                         </span>
                     </div>
                     <div className="text-right flex flex-col">
-                        <span className="text-lg font-bold">{label}</span>
-                        <span className="text-base">{value}</span>
+                        <span className="text-lg font-bold">{value}</span>
+                        <span className="text-base">{label}</span>
                     </div>
-
                 </CardContent>
-
             </Card>
-            <div className="relative flex items-center">
-                
-                {separator && (
-                    <div className="hidden md:block w-[1px] h-full  bg-gray-200 rounded-md " />
 
-                )}
-            </div>
-
+            {separator && (
+                <div className="hidden md:block w-[1px] h-[70px] bg-gray-200 rounded-md" />
+            )}
         </>
     );
 }
